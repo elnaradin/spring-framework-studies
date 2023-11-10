f82521c49fe032ddbdb779b1fbaf87706d25cca4
Эльнара Габдрахманова <elnaradin@gmail.com>
2023-11-10 21:57:15 +0500

    add test profile

--- 4_RestAPI_with_Database/newsservice/src/test/java/com/example/newsservice/AbstractTestController.java
+++ 4_RestAPI_with_Database/newsservice/src/test/java/com/example/newsservice/AbstractTestController.java
@@ -8,6 +8,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
@@ -15,13 +16,13 @@ import java.util.List;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class AbstractTestController {
     @Autowired
     protected MockMvc mockMvc;
     @Autowired
     protected ObjectMapper objectMapper;
     private final LocalDateTime someTime = LocalDateTime.of(2000, 1, 1, 0, 0, 0);
-
 
     protected News createNews(Long id, User author, List<Category> categoryList) {
         return News
